994d4b9c6e67ae35c755077ab006e7f58bbf4e60
richasojitra <154963674+richasojitra@users.noreply.github.com>
2024-01-04 20:08:00 +0530

    Add files via upload

---
+++ php1.php
@@ -0,0 +1,25 @@
+<?php
+$username=$_REQUEST['uname'];
+$password=$_REQUEST['password'];
+$name=$_REQUEST['name'];
+$address=$_REQUEST['address'];
+$country=$_REQUEST['drop-down'];
+$zipcode=$_REQUEST['ZIP_code'];
+$email=$_REQUEST['email'];
+$gender=$_REQUEST['gender'];
+$language=$_REQUEST['language'];
+$about=$_REQUEST['about'];
+
+echo "username=".$username."<br><br>";
+echo "password=".$password."<br><br>";
+echo "name=".$name."<br><br>";
+echo "address=".$address."<br><br>";
+echo "country=".$country."<br><br>";
+echo "zipcode=".$zipcode."<br><br>";
+echo "email=".$email."<br><br>";
+echo "gender=".$gender."<br><br>";
+echo "language=".$language."<br><br>";
+echo "about=".$about."<br><br>";
+
+
+?>
